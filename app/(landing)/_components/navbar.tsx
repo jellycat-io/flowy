7e0625b6683428a847aeb2345eff2f5616f033e2
salty-max <max@jellycat.fr>
2024-04-21 20:07:22 +0200

    fix: build

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
   return (
     <nav className='sticky top-0 w-full z-20 h-14 px-4 border-b shadow-sm flex items-center bg-background'>
       <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
-        <Logo isFull withLabel hideOnMobile />
+        <Logo withLabel hideOnMobile />
         <div className='flex space-x-4 md:w-auto items-center justify-between w-full'>
           <Button size='sm' asChild>
             <Link href={Routes.auth.login}>
